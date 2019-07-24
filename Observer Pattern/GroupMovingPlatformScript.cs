c4cb26769ba0599611295b9f7a01f463be16eebc
ashiqursuperfly <ashiqur.superfly@gmail.com>
2019-07-24 11:28:55 +0600

    Observer Pattern added :tada:

---
+++ Observer Pattern/GroupMovingPlatformScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupMovingPlatformScript : MonoBehaviour {
+    private MovingPlatformGroupScript mpgs;
+    void OnEnable() {
+        mpgs = transform.parent.GetComponent<MovingPlatformGroupScript>();
+        if (mpgs == null) {
+            Debug.Log(SharedConstantsAndUtils.DEBUG_TAG + "ERROR : mpgs null");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<PlayerTopScript>() == null &&
+         other.gameObject.GetComponent<PlayerFeetScript>() != null
+          && !PlayerParams.isOnJumpItemBoost) {
+            mpgs.isCollisionDetected = true;
+        }
+    }
+}
---
+++ Observer Pattern/MovingPlatformGroupScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingPlatformGroupScript : MonoBehaviour {
+    // [HideInInspector]
+    [HideInInspector] public bool isCollisionDetected = false;
+#pragma warning disable
+    [SerializeField] private List<GameObject> groupMovePlatforms;
+#pragma warning enable
+
+    int dir = 0;
+    void Update() {
+        if (isCollisionDetected) {
+            movePlatforms();
+            isCollisionDetected = false;
+        }
+    }
+    public void movePlatforms() {
+        foreach (GameObject g in groupMovePlatforms) {
+            if (g != null && g.GetComponent<GroupMovingPlatformScript>() != null) {
+                dir = Random.Range(0, 2);
+                if (dir == 0) g.transform.DOLocalMoveX(g.transform.position.x + 0.12f, 0.5f);
+                else g.transform.DOLocalMoveX(g.transform.position.x - 0.12f, 0.5f);
+            }
+        }
+
+    }
+}

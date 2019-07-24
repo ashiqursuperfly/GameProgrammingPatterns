e51f9ac991a5f9b342c990ee959ae4eb10431308
ashiqursuperfly <ashiqur.superfly@gmail.com>
2019-07-24 11:29:37 +0600

    Object Pooling Optimization:rocket:

---
+++ Object Pooling/CanonBallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class CanonBallScript : MonoBehaviour {
+    void OnEnable() {
+        transform.position = transform.parent.position;
+        int duration = Random.Range(3, 7);
+        int dir = transform.parent.position.x < 0 ? 1 : -1;
+        if (dir == -1) transform.GetComponent<SpriteRenderer>().flipX = true;
+        transform.DOMoveX(GamePlayParameters.screenBounds.x * dir, duration).SetEase(Ease.Linear).OnComplete(
+            () => { transform.parent.GetComponent<CanonScript>().addToPool(this.gameObject); }
+        );
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponent<PlayerTopScript>() != null && !PlayerParams.isOnShield && !PlayerParams.isOnJumpItemBoost) {
+            //destroy player
+            other.gameObject.transform.parent.DOPause();
+            other.transform.parent.GetChild(0).gameObject.SetActive(false);
+            other.transform.parent.GetChild(1).gameObject.SetActive(false);
+            DOTween.Sequence()
+            .Append(other.gameObject.transform.parent.DOLocalJump(other.gameObject.transform.parent.position + Vector3.up * 2f, 1f, 1, 1f))
+            .Insert(0.25f, other.gameObject.transform.parent.DOLocalRotate(new Vector3(-180, 0, -30), 0.5f, RotateMode.FastBeyond360))
+            .Append(other.gameObject.transform.parent.DOMove(new Vector3(0, -2 * GamePlayParameters.screenBounds.y, 0), 3f)).SetEase(Ease.Linear)
+            .OnComplete(() => {
+                GamePlayParameters.isGameover = true;
+            });
+        }
+    }
+}
---
+++ Object Pooling/CanonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CanonScript : MonoBehaviour {
+
+#pragma warning disable
+    [SerializeField] private GameObject canonBall;
+#pragma warning enable
+    private int maxCannonBalls = 2, poolSize;
+    private float interval = 3f;
+    Queue<GameObject> pool;
+    void OnEnable() {
+        pool = new Queue<GameObject>();
+        growPool();
+    }
+    private float t = 0;
+    void Update() {
+        t += Time.deltaTime;
+        poolSize = pool.Count;
+        if (t >= interval) {
+            t = 0;
+            if (pool.Count != 0) {
+                GameObject g = pool.Dequeue();
+                g.SetActive(true);
+                g.GetComponent<CanonBallScript>().enabled = true;
+            } else growPool();
+        }
+    }
+    public void addToPool(GameObject g) {
+        g.GetComponent<CanonBallScript>().enabled = false;
+        g.SetActive(false);
+        pool.Enqueue(g);
+    }
+    void growPool() {
+        for (int i = 0; i < maxCannonBalls; i++) {
+            GameObject g = Instantiate(canonBall, this.transform.position + (Vector3.right * 0.25f), this.transform.rotation);
+            g.transform.SetParent(this.transform);
+            pool.Enqueue(g);
+        }
+    }
+
+}
